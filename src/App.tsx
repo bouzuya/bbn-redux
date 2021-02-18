9b24c02a7f420fb13b16437e2c77c28c313b9645
bouzuya <m@bouzuya.net>
2021-02-19 06:29:55 +0900

    remove logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import { PostList } from "./features/posts/PostList";
@@ -10,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         {date === null ? null : <PostDetail date={date} />}
         <PostList onClickPost={(date) => setDate(date)} />
